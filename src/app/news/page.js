1a6f6e7b0f271938b0b117b4e0ca2070c68c34a2
Munir <smsirajulmonir@gmail.com>
2024-03-09 11:06:06 +0600

    nested route explored

---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h2>This is nested news page</h2>
+        </div>
+    );
+};
+
+export default NewsPage;
